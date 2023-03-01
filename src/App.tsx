d0efe70670c1399964e4f92ff6e9ecbc4a127af9
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-03-01 23:37:20 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    /*const names = ['Dimych', 'Sveta', 'Katya', 'Vicktor', 'Ignat'];*/
+
+    const users = [{name: 'Dimych'}, {name: 'Sveta'}, {name: 'Katya'}, {name: 'Vicktor'}, {name: 'Ignat'}]
+
+    const liElements = users.map((u, index) => <li key={index}>{index} - {u.name}</li>)
+    return (
+        <div className="App">
+            <ul>
+                {liElements}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
